6749890ca88e97c29b6dc55c910b14d426f19d8b
meriem <elounimeriem@gmail.com>
2023-05-02 09:48:27 +0100

    first commit

--- src/Pages/Description.js
+++ src/Pages/Description.js
@@ -1,18 +1,18 @@
-import React, { useState , useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { moviesData } from '../Components/Data'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { moviesData } from "../Components/Data";
 const Description = () => {
-    const [descrip , setDescrip] = useState({})
-    const {id} = useParams()
-    useEffect (()=>{
-        const movieD = moviesData.find((el)=>el.id==id);
-        setDescrip(movieD);
-    })
-  return (
-    <div>
-<h1>{descrip.description}</h1>
-    </div>
-  )
-}
+	const [descrip, setDescrip] = useState({});
+	const { id } = useParams();
+	useEffect(() => {
+		const movieD = moviesData.find((el) => el.id == id);
+		setDescrip(movieD);
+	});
+	return (
+		<div>
+			<h1>{descrip.description}</h1>
+		</div>
+	);
+};
 
-export default Description
+export default Description;
